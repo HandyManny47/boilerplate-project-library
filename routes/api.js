38643cb34a2bafcd84e8e51f8a622ae261383acd
Manny Diaz <mannyd47@gmail.com>
2024-06-10 23:06:15 +0000

    Book update

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,78 @@
 */
 
 'use strict';
+const { MongoClient, ObjectId } = require('mongodb');
 
-module.exports = function (app) {
 
+
+// Create a new MongoClient
+const client = new MongoClient(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the MongoDB server
+async function main() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    console.log('Connected to the MongoDB server');
+
+    // You can now interact with the database using the client object
+    const database = client.db('mydatabase');
+    
+    // For example, you can access collections like this:
+    const usersCollection = database.collection('users');
+
+    // Perform database operations...
+
+  } catch (error) {
+    console.error('Error connecting to the MongoDB server', error);
+  } finally {
+    // Close the connection when you're done
+    await client.close();
+    console.log('Connection closed');
+  }
+}
+
+
+
+module.exports = async function (app) {
+
+  try{
+    await client.connect();
+    console.log('Connected to the MongoDB server');
+  } catch (error) {
+    console.error('Error connecting to the MongoDB server', error);
+  } finally {
+    // Close the connection when you're done
+    await client.close();
+    console.log('Connection closed');
+  }
+  
   app.route('/api/books')
-    .get(function (req, res){
+    .get(async function (req, res){
       //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments
+      
+     // You can now interact with the database using the client object
+     const database = client.db('mydatabase');
+    
+     // For example, you can access collections like this:
+     const booksCollection = database.collection('books');
+
+   
+
+      res.send (await booksCollection.find().toArray())
     })
     
     .post(function (req, res){
       let title = req.body.title;
+      const database = client.db('mydatabase');
+    
+     
+     const booksCollection = database.collection('books');
+      const newBook={_id: new ObjectId(), title: title}
+      booksCollection.insertOne(newBook)
+      res.send (newBook)
+
       //response will contain new book object including atleast _id and title
     })
     
